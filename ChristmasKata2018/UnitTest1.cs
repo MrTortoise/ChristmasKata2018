2ce19935e5d5ade57e93441e362e0c671b6e9a35
John Nicholas <themrtortoise@gmail.com>
2018-12-13 10:12:53 +0000

    a starting point

--- ChristmasKata2018/UnitTest1.cs
+++ ChristmasKata2018/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace ChristmasKata2018
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void AStartingPoint()
         {
             var factory = new CustomLetterHandlerFactory("some", "values");
             Assert.NotNull(factory.CreateLetterHandler(new RequestContext(), "test" ));
